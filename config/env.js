d1bbdbaadb3a9bab7416354f9324d7a92e8acf5d
Tim Marler <timmy.marler@gmail.com>
2025-11-19 18:40:40 +0000

    Include env.js for EAS cloud builds

---
+++ config/env.js
@@ -0,0 +1,15 @@
+// =========================
+//  FIREBASE CONFIG
+// =========================
+
+module.exports = {
+  FIREBASE_API_KEY: "AIzaSyCreZstfHnXYWJx5QfaZdcWvoxNCMyLDHg",
+  FIREBASE_AUTH_DOMAIN: "coffee-rider-bea88.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "coffee-rider-bea88",
+  FIREBASE_STORAGE_BUCKET: "coffee-rider-bea88.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID: "1001945286149",
+  FIREBASE_APP_ID: "1:1001945286149:web:93cae68a7354a0dd1e7e6c",
+  GOOGLE_MAPS_API_KEY: "AIzaSyCJwgXEMFnD39fSiDkYVj4Falu9IpUGNO0",
+  GOOGLE_PLACES_API_KEY: "AIzaSyCreZstfHnXYWJx5QfaZdcWvoxNCMyLDHg",
+  STRIPE_PUBLISHABLE_KEY: "pk_test_your_key_here",
+};
