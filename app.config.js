f525671448d228ce7e2f1c5fa140c33302d415f7
Lauren Marler <laurenmarler@Laurens-MacBook-Air.local>
2026-02-17 15:24:57 +0000

    Update app.config.js version to 2.9.0

--- app.config.js
+++ app.config.js
@@ -57,7 +57,7 @@ export default {
     name: DISPLAY_NAME[APP_NAME],
     slug: SLUG[APP_NAME],
     scheme: SLUG[APP_NAME],
-    version: "2.8.1",
+    version: "2.9.0",
     icon: `${brandFolder}/icon.png`,
     splash: {
       image: `${brandFolder}/splash.png`,
