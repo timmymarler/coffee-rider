72a11f7659adb5d4c52423a4b54c7b4ddcf0ec48
Lauren Marler <laurenmarler@Laurens-MacBook-Air.local>
2026-02-21 15:51:37 +0000

    Bump version to 2.9.4 - mini map improvements, location polling optimization, and syntax fixes

--- app.config.js
+++ app.config.js
@@ -57,7 +57,7 @@ export default {
     name: DISPLAY_NAME[APP_NAME],
     slug: SLUG[APP_NAME],
     scheme: SLUG[APP_NAME],
-    version: "2.9.3",
+    version: "2.9.4",
     icon: `${brandFolder}/icon.png`,
     splash: {
       image: `${brandFolder}/splash.png`,
@@ -87,7 +87,7 @@ export default {
 
     ios: {
       bundleIdentifier: IOS_BUNDLE[APP_NAME],
-      buildNumber: "6",
+      buildNumber: "7",
       googleServicesFile: "./GoogleService-Info.plist",
       infoPlist: {
         NSLocationWhenInUseUsageDescription: "Coffee Rider needs your location to show nearby places and help with group rides.",
