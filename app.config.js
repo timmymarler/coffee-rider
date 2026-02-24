266447f4944980477290622497ce9ac6f9b4749b
Lauren Marler <laurenmarler@Laurens-MacBook-Air.local>
2026-02-24 08:25:46 +0000

    Bump build numbers to 3 for new release

--- app.config.js
+++ app.config.js
@@ -87,7 +87,7 @@ export default {
 
     ios: {
       bundleIdentifier: IOS_BUNDLE[APP_NAME],
-      buildNumber: "2",
+      buildNumber: "3",
       googleServicesFile: "./GoogleService-Info.plist",
       infoPlist: {
         NSLocationWhenInUseUsageDescription: "Coffee Rider needs your location to show nearby places and help with group rides.",
@@ -101,7 +101,7 @@ export default {
 
     android: {
       package: ANDROID_PACKAGE[APP_NAME],
-      versionCode: 2,
+      versionCode: 3,
       permissions: [
         "android.permission.ACCESS_FINE_LOCATION",
         "android.permission.ACCESS_COARSE_LOCATION",
