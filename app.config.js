7d29356d0f8366950311d519ca38b5705c9fe53c
Lauren Marler <laurenmarler@Laurens-MacBook-Air.local>
2026-03-02 09:09:12 +0000

    Version 2.10.2 Build 1 - Clean build after deployment
    - Update version from 2.10.1 to 2.10.2
    - Reset build numbers to 1 (iOS buildNumber and Android versionCode)
    - Ready for new deployment cycle with fresh build numbers

--- app.config.js
+++ app.config.js
@@ -57,7 +57,7 @@ export default {
     name: DISPLAY_NAME[APP_NAME],
     slug: SLUG[APP_NAME],
     scheme: SLUG[APP_NAME],
-    version: "2.10.1",
+    version: "2.10.2",
     icon: `${brandFolder}/icon.png`,
     splash: {
       image: `${brandFolder}/splash.png`,
@@ -87,7 +87,7 @@ export default {
 
     ios: {
       bundleIdentifier: IOS_BUNDLE[APP_NAME],
-      buildNumber: "7",
+      buildNumber: "1",
       googleServicesFile: "./GoogleService-Info.plist",
       infoPlist: {
         NSLocationWhenInUseUsageDescription: "Coffee Rider needs your location to show nearby places and help with group rides.",
@@ -101,7 +101,7 @@ export default {
 
     android: {
       package: ANDROID_PACKAGE[APP_NAME],
-      versionCode: 7,
+      versionCode: 1,
       permissions: [
         "android.permission.ACCESS_FINE_LOCATION",
         "android.permission.ACCESS_COARSE_LOCATION",
