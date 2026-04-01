ff6371df9e65dbc04762d9fa57d1c349592e9d74
Lauren Marler <laurenmarler@Laurens-MacBook-Air.local>
2026-04-01 18:50:49 +0100

    Bump version to 2.21.1

--- app.config.js
+++ app.config.js
@@ -67,7 +67,7 @@ export default {
     name: DISPLAY_NAME[APP_NAME],
     slug: SLUG[APP_NAME],
     scheme: SLUG[APP_NAME],
-    version: "2.21.0",
+    version: "2.21.1",
     icon: `${brandFolder}/icon.png`,
     splash: {
       image: `${brandFolder}/splash.png`,
