10a5d3d8753eb4433a1397cbd8f0086e3c61cb8b
Lauren Marler <laurenmarler@Laurens-MacBook-Air.local>
2026-05-29 15:10:20 +0100

    Bump build numbers to 54

--- app.config.js
+++ app.config.js
@@ -114,7 +114,7 @@ export default {
 
     ios: {
       bundleIdentifier: IOS_BUNDLE[APP_NAME],
-      buildNumber: "53",
+      buildNumber: "54",
       googleServicesFile: "./GoogleService-Info.plist",
       infoPlist: {
         NSLocationWhenInUseUsageDescription: "Coffee Rider needs your location to show nearby places and help with group rides.",
@@ -130,7 +130,7 @@ export default {
 
     android: {
       package: ANDROID_PACKAGE[APP_NAME],
-      versionCode: 53,
+      versionCode: 54,
       permissions: [
         "android.permission.ACCESS_FINE_LOCATION",
         "android.permission.ACCESS_COARSE_LOCATION"
