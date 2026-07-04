38419e78cf34958b543130a8d2ad7d30521c4709
Lauren Marler <laurenmarler@Laurens-MacBook-Air.local>
2026-07-04 18:28:59 +0100

    Bump app version to 2.31.42 (99)

--- app.config.js
+++ app.config.js
@@ -69,7 +69,7 @@ export default {
     name: DISPLAY_NAME[APP_NAME],
     slug: SLUG[APP_NAME],
     scheme: SLUG[APP_NAME],
-    version: "2.31.41",
+    version: "2.31.42",
     icon: `${brandFolder}/icon.png`,
     splash: {
       image: `${brandFolder}/splash.png`,
@@ -114,7 +114,7 @@ export default {
 
     ios: {
       bundleIdentifier: IOS_BUNDLE[APP_NAME],
-      buildNumber: "98",
+      buildNumber: "99",
       googleServicesFile: "./GoogleService-Info.plist",
       infoPlist: {
         NSLocationWhenInUseUsageDescription: "Coffee Rider needs your location to show nearby places and help with group rides.",
@@ -130,7 +130,7 @@ export default {
 
     android: {
       package: ANDROID_PACKAGE[APP_NAME],
-      versionCode: 98,
+      versionCode: 99,
       permissions: [
         "android.permission.ACCESS_FINE_LOCATION",
         "android.permission.ACCESS_COARSE_LOCATION"
